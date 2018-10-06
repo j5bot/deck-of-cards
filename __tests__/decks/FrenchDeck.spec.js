9851c1f86de31d3b2f968c3d01888ff3ec4d89c1
Jonathan ‘J5’ Cook <jonathan.j5.cook@gmail.com>
2018-10-06 13:54:52 -0400

    Update to remove duplicate tests per CodeFactor

--- __tests__/decks/FrenchDeck.spec.js
+++ __tests__/decks/FrenchDeck.spec.js
@@ -1,7 +1,4 @@
-import { PlayingCard } from '../../src/cards/FrenchPlayingCard.js';
 import { Deck } from '../../src/decks/FrenchDeck.js';
-import { Suits } from '../../src/cards/Suits.js';
-import { names } from '../../src/cards/french-deck-names.js';
 
 describe('card deck', () => {
 
@@ -20,45 +17,6 @@ describe('card deck', () => {
 
     test('iterator', () => {
       expect(deck.iterator()).toBeDefined();
-    });
-
-    describe('card values', () => {
-
-      const iterator = deck.iterator();
-      const suits = Suits.elements;
-
-      const testEachCard = (suit, suitIndex, name, nameIndex) => {
-
-        // if (nameIndex === 0) {
-        //   return;
-        // }
-
-        const expectCard = new PlayingCard(
-          nameIndex, suitIndex
-        );
-
-        const card = iterator.next().value;
-
-        test(expectCard.description, () => {
-          expect(card).toEqual(expectCard);
-        });
-
-      };
-
-      suits.forEach(
-        (suit, suitIndex) => {
-
-          names.forEach(
-            (name, nameIndex) => {
-
-              testEachCard(suit, suitIndex, name, nameIndex);
-
-            }
-          );
-
-        }
-      );
-
     });
 
   });
