4e3b2a97c95618f62dcb84cd5263c1fd7c88b93a
Jonathan ‘J5’ Cook <jonathan.j5.cook@gmail.com>
2018-10-10 15:14:23 -0400

    Add scripts to demo site

--- demo/assets/scripts/test-reporter.js
+++ demo/assets/scripts/test-reporter.js
@@ -1,83 +1,80 @@
-'use strict';
+import React from 'react';
+import {createHierarchy} from './create-hierarchy.js';
 
-/* From https://codepen.io/konsumer/pen/ObKBWO */
-var basename = function basename(path) {
-  return path.split('/').pop();
+export const basename = path => path.split(
+  (path.indexOf('__tests__') < 0) ? '/' : '__tests__'
+).pop();
+
+export const StatusIcon = ({status}) => <i>{status === 'passed' ? '✔' : '✘'}</i>;
+
+export const Spec = ({result}) => {
+
+  return (<div className={'Result ' + result.status}>
+    <h2>
+      <StatusIcon status={result.status} />
+      {basename(result.name)}
+    </h2>
+      { result.summary && (<h3>{result.summary}</h3>) }
+      <NestedResults test={result}/>
+    </div>);
 };
 
-var StatusIcon = function StatusIcon(_ref) {
-  var status = _ref.status;
-  return React.createElement(
-    'i',
-    null,
-    status === 'passed' ? '✔' : '✘'
+export const NestedResult = ({assertion}) => {
+  return (
+    <div className={'assertion ' + assertion.status}>
+      <StatusIcon status={assertion.status} />
+      <span className="assertion-title">{assertion.title}</span>
+      { assertion.failureMessages && assertion.failureMessages.map(msg => (
+        <pre>{msg}</pre>
+      )) }
+    </div>
   );
 };
 
-var Result = function Result(_ref2) {
-  var result = _ref2.result;
-  return React.createElement(
-    'div',
-    { className: 'Result ' + result.status },
-    React.createElement(
-      'h2',
-      null,
-      basename(result.name)
-    ),
-    React.createElement(
-      'h3',
-      null,
-      result.summary
-    ),
-    result.assertionResults.map(function (assertion) {
-      return React.createElement(
-        'div',
-        { className: 'assertion ' + assertion.status },
-        React.createElement(StatusIcon, { status: assertion.status }),
-        ' ',
-        assertion.title,
-        assertion.failureMessages && assertion.failureMessages.map(function (msg) {
-          return React.createElement(
-            'pre',
-            null,
-            msg
-          );
-        })
-      );
-    })
-  );
+export const ResultGroup = ({group}) => (
+  <div className="resultGroup">
+    <span>{group.title}</span>
+    { group.tests.map( (test,i) => <NestedResults test={test} key={i}/> ) }
+  </div>
+);
+
+export const NestedResults = ({test}) => {
+  return test && (
+    (test.tests && test.tests.length > 0) ?
+      (<ResultGroup group={test}/>) :
+        (<NestedResult assertion={test}/>)
+      ) || null;
+
+  return elements;
 };
 
-var Header = function Header(_ref3) {
-  var results = _ref3.results;
-  return React.createElement(
-    'header',
-    null,
-    results.numFailedTestSuites && React.createElement(
-      'div',
-      { title: results.numFailedTestSuites + ' failing suites', className: 'metric failed' },
-      results.numFailedTestSuites
-    ),
-    results.numPassedTestSuites && React.createElement(
-      'div',
-      { title: results.numPassedTestSuites + ' passing suites', className: 'metric passed' },
-      results.numPassedTestSuites
-    )
-  );
-};
+export const Header = ({results}) => (
+  <header>
+    {results.numFailedTestSuites !== 'undefined' && <div title={`${results.numFailedTestSuites} failing suites`} className='metric failed'>{results.numFailedTestSuites}</div>}
+    {results.numPassedTestSuites !== 'undefined' && <div title={`${results.numPassedTestSuites} passing suites`} className='metric passed'>{results.numPassedTestSuites}</div>}
+  </header>
+);
 
-var App = function App(_ref4) {
-  var results = _ref4.results;
-  return React.createElement(
-    'div',
-    null,
-    React.createElement(Header, { results: results }),
-    React.createElement(
-      'div',
-      null,
-      results.testResults.map(function (result, i) {
-        return React.createElement(Result, { result: result, key: i });
-      })
-    )
-  );
-};
+export const App = ({results, config}) => (
+  <div>
+    <Header results={results} />
+    <div>
+      {
+        results.testResults
+          .sort((a, b) => {
+            return a.name === b.name ? 0 : (a.name < b.name ? -1 : 1)
+          })
+          .map((result, i) => {
+            const root = createHierarchy(result, config).tests[0];
+            root.name = result.name;
+            root.summary = result.summary;
+            root.status = result.status;
+
+            return (
+              <Spec result={root} key={i} />
+            );
+          })
+      }
+    </div>
+  </div>
+);
