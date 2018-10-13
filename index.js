6e9a1573d2118086c5c6dd14446a8e624327bab2
Jonathan ‘J5’ Cook <jonathan.j5.cook@gmail.com>
2018-10-13 07:40:36 -0400

    Update to export classes and functions

---
+++ index.js
@@ -0,0 +1,40 @@
+// card-sets
+import BaseCardSet from './src/card-sets/BaseCardSet.js';
+
+// cards
+import BaseCard from './src/cards/BaseCard.js';
+import names from './src/cards/french-deck-names.js';
+import values from './src/cards/french-deck-values.js';
+import PlayingCard from './src/cards/FrenchPlayingCard.js';
+import Suits from './src/cards/Suits.js';
+
+// dealer
+import dealer from './src/dealer/dealer.js';
+
+// decks
+import DealableDeck from './src/decks/DealableFrenchDeck.js';
+import Deck from './src/decks/FrenchDeck.js';
+import ShufflableDeck from './src/decks/ShufflableFrenchDeck.js';
+
+// shuffler
+import shuffler from './src/shuffler/shuffler.js';
+
+// util
+import Enum from './src/util/Enum.js';
+import getRandomInt from './src/util/random.js';
+
+export default {
+  BaseCardSet,
+  BaseCard,
+  PlayingCard,
+  names,
+  values,
+  Suits,
+  dealer,
+  DealableDeck,
+  Deck,
+  ShufflableDeck,
+  shuffler,
+  Enum,
+  getRandomInt
+};
